64f0f8316098fe66dba578dd6f7a29815ced56a8
JCTalbott <jctalbott2@gmail.com>
2021-02-27 12:37:33 -0500

    seeds for drive

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "google_drive"
+
+# Creates a session. This will prompt the credential via command line for the
+# first time and save it to config.json file for later usages.
+# See this document to learn how to create config.json:
+# https://github.com/gimite/google-drive-ruby/blob/master/doc/authorization.md
+session = GoogleDrive::Session.from_config("config.json")
+
+# First worksheet of
+# https://docs.google.com/spreadsheet/ccc?key=pz7XtlQC-PYx-jrVMJErTcg
+# Or https://docs.google.com/a/someone.com/spreadsheets/d/pz7XtlQC-PYx-jrVMJErTcg/edit?usp=drive_web
+ws = session.spreadsheet_by_key("160ZPXSLu2PEWggDYos-f4P2SBePKuAJvxB9p2Po7nSI").worksheets[0]
+
+# Gets content of A2 cell.
+p ws[2, 1]  #==> "hoge"
+
+# Changes content of cells.
+# Changes are not sent to the server until you call ws.save().
+ws[2, 1] = "foo"
+ws[2, 2] = "bar"
+ws.save
+
+# Dumps all cells.
+(1..ws.num_rows).each do |row|
+  (1..ws.num_cols).each do |col|
+    p ws[row, col]
+  end
+end
+
+# Yet another way to do so.
+p ws.rows  #==> [["fuga", ""], ["foo", "bar]]
+
+# Reloads the worksheet to get changes by other clients.
+ws.reload
